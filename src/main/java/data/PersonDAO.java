5b12bec2a8cc1873ab6950a6edeeb2196d37cc79
JhordP <j.horda.ny@hotmail.com>
2022-07-20 01:15:14 -0400

    update function done in personDAO [data]

--- src/main/java/data/PersonDAO.java
+++ src/main/java/data/PersonDAO.java
@@ -8,6 +8,8 @@ import domain.Person;
 public class PersonDAO { //DAO: Data Access Object
     private static final String SQL_SELECT = "SELECT * FROM person;";
     private static final String SQL_INSERT = "INSERT INTO person(person_name, person_lastname, person_email, person_phone) VALUES(?, ?, ?, ?)";
+    private static final String SQL_UPDATE = "UPDATE person SET person_name = ?, person_lastname = ?, person_email = ?, person_phone = ? WHERE (person_id = ?)";
+    private static final String SQL_DELETE = "DELETE FROM person WHERE id = ?";
 
     public List<Person> select() {
         Connection connection = null;
@@ -70,9 +72,42 @@ public class PersonDAO { //DAO: Data Access Object
                 DatabaseConnection.close(con);
             } catch (SQLException e) {
                 System.out.println("An error has ocurred trying to close the Data Base connection. "+e.getMessage());
-                e.printStackTrace();
+                e.printStackTrace(System.out);
             }
         }
         return numRecords;   
     }
+
+    public int update(Person person) {
+        Connection con = null;
+        PreparedStatement statement = null;
+        int numRecords = 0;
+
+        try {
+            con = DatabaseConnection.getConnection();
+            statement = con.prepareStatement(SQL_UPDATE);
+
+            statement.setString(1, person.getPersonName());
+            statement.setString(2, person.getPersonLastname());
+            statement.setString(3, person.getPersonEmail());
+            statement.setLong(4, person.getPersonPhone());
+
+            statement.setInt(5, person.getPersonId()); //Parameter Index is NOT the column number. It is the number of "?" sign in the string
+            numRecords = statement.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println("An error has ocurred trying to reach the Data Base. "+e.getMessage());
+            e.printStackTrace(System.out);
+        } finally {
+            try {
+                DatabaseConnection.close(statement);
+                DatabaseConnection.close(con);
+            } catch (SQLException e) {
+                System.out.println("An error has ocurred trying to close the Data Base connection. "+e.getMessage());
+                e.printStackTrace(System.out);
+            }
+        }
+        return numRecords;   
+    }
+
 }
--- src/main/java/test/TestPersonDrive.java
+++ src/main/java/test/TestPersonDrive.java
@@ -13,20 +13,25 @@ public class TestPersonDrive {
         for (Person person : personList) {
             System.out.println(person);
         }
-        System.out.print("Name: "); String name = input.next();
-        System.out.print("Last Name: "); String lastName = input.next();
-        System.out.print("E-Mail: "); String email = input.next();
-        System.out.print("Phone: "); Long phone = input.nextLong();
+        // System.out.print("Name: "); String name = input.next();
+        // System.out.print("Last Name: "); String lastName = input.next();
+        // System.out.print("E-Mail: "); String email = input.next();
+        // System.out.print("Phone: "); Long phone = input.nextLong();
         input.close();
-        
+
         System.out.println("After Updating:---");
         //Inserting
-        var person = new Person(name, lastName, email, phone);
-        personDAO.insert(person);
+        // var person = new Person(name, lastName, email, phone);
+        // personDAO.insert(person);
+
+        // personList = personDAO.select();
+        // personList.forEach(p -> {System.out.println(p);});
+
+        //Updating
+        var person = new Person(5, "Jhederly", "Polanco", "jhederlypolanco@gmail.com", 8493992117L);
+        personDAO.update(person);
 
         personList = personDAO.select();
         personList.forEach(p -> {System.out.println(p);});
-
-
     }
 }
