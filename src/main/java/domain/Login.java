44e262853269ae1d9b03ca4c9c001498b42261f1
JhordP <j.horda.ny@hotmail.com>
2022-07-20 15:47:58 -0400

    Login class Created [domain]

---
+++ src/main/java/domain/Login.java
@@ -0,0 +1,45 @@
+package domain;
+
+public class Login {
+
+    //Columns
+    private int id;
+    private String username;
+    private String passw;
+
+    public Login() {}
+    public Login(int id) {
+        this.id = id;
+    }
+    public Login(String username, String passw) {
+        this.username = username;
+        this.passw = passw;
+    }
+    
+    //Getters & Setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassw() {
+        return passw;
+    }
+    public void setPassw(String passw) {
+        this.passw = passw;
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        return "Login [id=" + id + ", username=" + username +", passw=" + passw + "]";
+    }
+    
+}
